9d69d42b71410ba2b11057ef0ace64dd91c424e2
Baixiang Huang <bhuang15@hawk.iit.edu>
2024-10-23 22:42:36 -0500

    update

--- code/util.py
+++ code/util.py
@@ -15,9 +15,7 @@ topic_ls = ['places_city', 'places_country', 'places_landmark', 'entertainment_a
             'art_literary', 'art_sculpture', 'health_treatment', 'health_medication', 'health_disease', 'human_politician', 'human_writer', 'human_scientist', 
             'event_sport', 'event_history', 'event_film']
 
-# 'lmsys/vicuna-7b-v1.5', 'google/gemma-2-9b-it', 'chavinlo/alpaca-native'
-model_id_ls = ['meta-llama/Meta-Llama-3-8B-Instruct', 'mistralai/Mistral-7B-Instruct-v0.3', 'meta-llama/Llama-2-7b-chat-hf', 
-               'google/gemma-1.1-2b-it', 'google/gemma-2-9b-it']
+model_id_ls = ['meta-llama/Meta-Llama-3-8B-Instruct', 'mistralai/Mistral-7B-Instruct-v0.3', 'meta-llama/Llama-2-7b-chat-hf']
 model_id_format_ls = [e.split('/')[-1].replace('-', '_').lower() for e in model_id_ls]
 
 
